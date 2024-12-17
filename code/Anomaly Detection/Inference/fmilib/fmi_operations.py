6a0a03e2ac98d6d980ffd95ae7e367c090e26844
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-12-17 10:05:45 -0500

    changes to re-introduce FMI

--- code/Anomaly Detection/Inference/fmilib/fmi_operations.py
+++ code/Anomaly Detection/Inference/fmilib/fmi_operations.py
@@ -3,7 +3,7 @@ import fmi
 
 def fmi_communicator(world_size, rank):
 
-    communicator = fmi.Communicator(rank, world_size, "fmi.json", "fmi_pair", 512)
+    communicator = fmi.Communicator(rank, world_size, "/tmp/Anomaly Detection/config/fmi.json", "fmi_pair", 512)
 
     if communicator is None:
         print("unable to create FMI Communicator")
